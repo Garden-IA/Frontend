be5b1ea51a84a383ada492080fded78f0d5cf3fe
JSisques <javierplazasisques@gmail.com>
2024-08-22 19:20:50 +0200

    v1.0.0

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,15 +1,14 @@
-// src/app/auth/layout.tsx
 import React from 'react';
-import '../../../styles/globals.css'; // Estilos globales
+import '../../../styles/globals.css';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <>
       <Header />
-      <main>{children}</main>
+      <div>{children}</div>
       <Footer />
-    </div>
+    </>
   );
 }
